541889bb39b864df4e7eeec30b53b22e065489fc
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-02-23 21:19:40 -0500

    Added files via upload

---
+++ scrabble.js
@@ -0,0 +1,32 @@
+
+
+var createGameBoard = function(rows,cols){
+	//create
+	var t = document.createElement("TABLE");
+	t.setAttribute("id","gameBoard");
+	
+	
+	
+	for(var i=0;i<rows;i++){
+
+		//create row and append to table
+		var r = document.createElement("TR");
+		var rowId = i.toString();
+		r.setAttribute("id",rowId);
+		t.appendChild(r);
+
+		for(var j=0;j<cols;j++){
+
+			var c = document.createElement("TD");
+			var cellId = rowId + j.toString();
+			c.setAttribute("id",cellId);
+			c.setAttribute("class","game-grid-cell");
+			t.appendChild(c);
+		}
+	}
+	document.getElementById("boardWrap").appendChild(t);
+}
+
+
+createGameBoard(10,10);
+
